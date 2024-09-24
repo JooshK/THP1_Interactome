3db4bb78d7826be23a79c29efa898f5316cc55d4
Joshua Khorsandi <49795222+JooshK@users.noreply.github.com>
2024-09-24 18:35:37 -0400

    Haplotype issue

--- GSE207487/THP1_DGE/edgeR_limma.R
+++ GSE207487/THP1_DGE/edgeR_limma.R
@@ -8,21 +8,19 @@ library(biomaRt)
 
 # Data Organization ----------------------------
 counts_matrix <- read.csv("../data/raw_counts_GRCh38_GREIS.csv") %>% 
-  dplyr::select(c(1:5, 7:9)) |> # compare uninfected to WT infected cells, triplicate
   column_to_rownames(var = "X")
+  # dplyr::select(c(1:5, 7:9)) |> # compare uninfected to WT infected cells, triplicate
 
 group <- as.factor(c(rep("UN", 3), rep("WT", 3)))
 x <- DGEList(counts_matrix, group = group)
 x$samples
 
 # first column contains old gene symbols, updated version for HGNC ids
-geneid <- rownames(x) # Entrez gene ids from Salmon
-genes <- select(Homo.sapiens, keys = geneid, columns = c("SYMBOL", "TXCHROM"), 
-                keytype = "ENSEMBL")
-genes <- genes[!duplicated(genes$ENSEMBL),]
+# geneid <- rownames(x) # Entrez gene ids from Salmon
+# genes <- select(Homo.sapiens, keys = geneid, columns = c("SYMBOL", "TXCHROM"), 
+                # keytype = "ENSEMBL")
+# genes <- genes[!duplicated(genes$ENSEMBL),]
 head(genes)
-
-x$genes <- genes
 x
 
 # Normalization and Filtering --------------------------------
@@ -94,14 +92,14 @@ glimmaVolcano(et, dge = x_nb)
 top_genes <- topTags(et, n = 100)
 lcpm_norm <- as.data.frame(cpm(x,log=TRUE))
 
-i <- which(et$genes$ENSEMBL %in% top_genes$table$ENSEMBL)
+i <- which(et$genes$gene_symbol %in% top_genes$table$gene_symbol)
 
 heat_plot <- pheatmap(lcpm_norm[i,], 
                       col = brewer.pal(10, 'RdYlGn'),
                       cluster_rows = T, cluster_cols = T, trace = "none",
                       angle_col = 45, 
                       fontsize_row = 6, fontsize_col = 8, show_rownames = T, 
-                      main = " Uninfected vs. WT THP1", labels_row = et$genes$SYMBOL[i], 
+                      main = " Uninfected vs. WT THP1", labels_row = et$genes$gene_symbol, 
                       labels_col = group)
 
 
@@ -120,12 +118,9 @@ write.table(topgenes_qval, "out/qval005_UNvsWT.txt", sep = "\t", row.names = F, 
 # GSEA ranks file 
 ranks_RNAseq = sign(tt_exact_test$table$logFC) * -log10(tt_exact_test$table$PValue)
 
-genenames <- tt_exact_test$table$SYMBOL
-geneids <- tt_exact_test$table$ENSEMBL
-
 # create the ranks file
-ranks_RNAseq <- cbind(genenames, ranks_RNAseq)
-colnames(ranks_RNAseq) <- c("GeneName","rank")
+ranks_RNAseq <- cbind(rownames(tt_exact_test$table), ranks_RNAseq)
+colnames(ranks_RNAseq) <- c("GeneID","rank")
 ranks_RNAseq <- ranks_RNAseq[order(as.numeric(ranks_RNAseq[,2]),decreasing = TRUE),]
 head(ranks_RNAseq) # the top2 values have pval ~ 0 so produce Inf rank
 
@@ -135,24 +130,10 @@ write.table(ranks_RNAseq, file = "out/UNvsWT.rnk", sep = "\t", quote = F, row.na
 options(RCurlOptions=list(followlocation=TRUE, postredir=2L)) # bio mart redirect 
 normalized_expression_RNAseq <- cpm(x)
 
-EM_expressionFile_RNAseq <- data.frame(Name = genenames, normalized_expression_RNAseq)
-rownames(EM_expressionFile_RNAseq) <- rownames(normalized_expression_RNAseq)
-
-#Add descriptions instead of geneids
-mart = useMart(biomart="ENSEMBL_MART_ENSEMBL")
-mart = useDataset(mart, dataset="hsapiens_gene_ensembl" )
-
-genes = getBM(attributes = c( 'hgnc_symbol', 'description'), filters='hgnc_symbol', 
-              values=genenames, mart=mart);
-genes$description = gsub("\\[Source.*", "", genes$description);
-
-EM_expressionFile_RNAseq <- merge(genes,EM_expressionFile_RNAseq,  
-                                  all.y=TRUE,by.x=1, by.y=1)
-colnames(EM_expressionFile_RNAseq)[1] <- "Name"
-colnames(EM_expressionFile_RNAseq)[2] <- "Description"
+EM_expressionFile_RNAseq <- data.frame(Description = x$genes$gene_symbol, normalized_expression_RNAseq) # set the description to be the symbol
 head(EM_expressionFile_RNAseq)
 
-write.table(EM_expressionFile_RNAseq ,file = "out/EM_expressionFile_RNAseq.txt", sep = "\t", row.names = F, quote = F)
+write.table(EM_expressionFile_RNAseq ,file = "out/EM_expressionFile_RNAseq.txt", sep = "\t", row.names = T, quote = F)
 
 
 
