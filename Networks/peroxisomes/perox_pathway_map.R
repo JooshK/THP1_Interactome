80d67604382cfb8e6cd5bbf15e2c969946d5d35e
Joshua Khorsandi <49795222+JooshK@users.noreply.github.com>
2024-10-02 23:34:36 -0400

    mapping code

--- Networks/peroxisomes/perox_pathway_map.R
+++ Networks/peroxisomes/perox_pathway_map.R
@@ -5,12 +5,13 @@ library(pathview)
 load("~/THP1_Interactome/GSE207487/THP1_DGE/out/DGEList_UNvsWT.Rdata")
 load("~/THP1_Interactome/GSE207487/THP1_DGE/out/exactTest_UNvsWT.Rdata")
 
-fatty_acid_cat_genes <- read.table("fatty_acid_catabolisim_autoAnn.txt", sep = '\t', header = T)
-genes <- fatty_acid_cat_genes$Gene
+perox_genes <- read.table("perox_genes.txt", sep = '\t', header = T)
+genes <- perox_genes$GeneID
 i <- which(rownames(et$table) %in% genes) # pull out FA gene logFC
-fa_expression <- et$table[i,]
-fa_expression$entrezid <- bitr(rownames(fa_expression), fromType="ENSEMBL", toType=c("ENTREZID"), OrgDb="org.Hs.eg.db")$ENTREZID
-head(fa_expression)
+perox_expression <- et$table[i,]
+
+perox_expression$entrezid <- bitr(rownames(perox_expression), fromType="ENSEMBL", toType=c("ENTREZID"), OrgDb="org.Hs.eg.db")$ENTREZID
+head(perox_expression)
 
 # KEGG ------------------
 # Enrichment
@@ -18,27 +19,18 @@ ids <- bitr(genes, fromType="ENSEMBL", toType=c("ENTREZID"), OrgDb="org.Hs.eg.db
 kk <- enrichKEGG(gene         = ids$ENTREZID,
                  organism     = 'hsa',
                  pvalueCutoff = 0.05)
-kk
+View(kk@result)
 
 # Visualization
 significant_pathways <- kk@result[kk@result$p.adjust < 0.01, ]
 enriched_pathways <- significant_pathways$ID
 
-fa_expression_vector <- setNames(fa_expression$logFC, fa_expression$entrezid)
-head(fa_expression_vector)
+perox_expression_vector <- setNames(perox_expression$logFC, perox_expression$entrezid)
+head(perox_expression_vector)
 
-for (pathway in enriched_pathways) {
-  tryCatch(
-    {
-    pathview(gene.data = fa_expression_vector,
-            pathway.id = pathway,
-            species = "hsa",
-            limit = list(gene = 2, cpd = 1), low = c("red", "red"), high = c("green", "green"), kegg.native = T)
-    }, 
-    error = function(cond) {
-      next
-    }
-  )
-}
+pathview(gene.data = perox_expression_vector,
+         pathway.id = "hsa03320",
+         species = "hsa",
+         limit = list(gene = 2, cpd = 1), low = c("red", "red"), high = c("green", "green"), kegg.native = T)
 
 
