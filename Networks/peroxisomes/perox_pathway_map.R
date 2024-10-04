6eb4cadceaec4e71012750256e54b89f6d9edb0d
JooshK <jokhorsandi@gmail.com>
2024-10-04 13:59:20 -0400

    Perox refactor

--- Networks/peroxisomes/perox_pathway_map.R
+++ Networks/peroxisomes/perox_pathway_map.R
@@ -1,22 +1,25 @@
 library(clusterProfiler)
 library(org.Hs.eg.db)
 library(pathview)
+library(tidyverse)
 
-load("~/THP1_Interactome/GSE207487/THP1_DGE/out/DGEList_UNvsWT.Rdata")
-load("~/THP1_Interactome/GSE207487/THP1_DGE/out/exactTest_UNvsWT.Rdata")
+load("../../GSE207487/THP1_DGE/out/DGEList_UNvsWT.Rdata")
+load("../../GSE207487/THP1_DGE/out/exactTest_UNvsWT.Rdata")
 
-perox_genes <- read.table("perox_genes.txt", sep = '\t', header = T)
-genes <- perox_genes$GeneID
-i <- which(rownames(et$table) %in% genes) # pull out FA gene logFC
+perox_genes <- read.csv("perox_lipid_genes.csv") |> 
+  column_to_rownames(var = "ENSEMBL")
+genes <- rownames(perox_genes)
+i <- which(rownames(et$table) %in% genes) # pull out gene logFC
 perox_expression <- et$table[i,]
 
-perox_expression$entrezid <- bitr(rownames(perox_expression), fromType="ENSEMBL", toType=c("ENTREZID"), OrgDb="org.Hs.eg.db")$ENTREZID
+perox_expression <- merge(perox_expression, perox_genes, by=0, all.x = T) |> 
+  column_to_rownames(var = "Row.names")
+
 head(perox_expression)
 
 # KEGG ------------------
 # Enrichment
-ids <- bitr(genes, fromType="ENSEMBL", toType=c("ENTREZID"), OrgDb="org.Hs.eg.db")
-kk <- enrichKEGG(gene         = ids$ENTREZID,
+kk <- enrichKEGG(gene         = perox_expression$ENTREZ_ACC,
                  organism     = 'hsa',
                  pvalueCutoff = 0.05)
 View(kk@result)
@@ -25,12 +28,11 @@ View(kk@result)
 significant_pathways <- kk@result[kk@result$p.adjust < 0.01, ]
 enriched_pathways <- significant_pathways$ID
 
-perox_expression_vector <- setNames(perox_expression$logFC, perox_expression$entrezid)
+perox_expression_vector <- setNames(perox_expression$logFC, perox_expression$ENTREZ_ACC)
 head(perox_expression_vector)
 
 pathview(gene.data = perox_expression_vector,
-         pathway.id = "hsa03320",
+         pathway.id = "hsa04146",
          species = "hsa",
          limit = list(gene = 2, cpd = 1), low = c("red", "red"), high = c("green", "green"), kegg.native = T)
-
 
